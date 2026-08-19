f184447e3ed200afba877c318ac85f34d43e7428
Vu241 <23017241@st.phenikaa-uni.edu.vn>
2026-08-19 11:24:46 +0700

    Update Test_issue2.java

--- src/test/java/Test_issue2.java
+++ src/test/java/Test_issue2.java
@@ -2,10 +2,37 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculatorStatementTest {
+public class CalculatorPathTest {
 
     @Test
-    void testSumEvenNumbers() {
+    void testLoopNotExecuted() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.sumEvenNumbers(0);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void testIfConditionTrue() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.sumEvenNumbers(2);
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    void testIfConditionFalse() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.sumEvenNumbers(1);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void testMultipleIterations() {
         Calculator calculator = new Calculator();
 
         int result = calculator.sumEvenNumbers(5);
