b547fad59861b8e21d0caaaeaab1ee9f47fe1b41
Vu241 <23017241@st.phenikaa-uni.edu.vn>
2026-08-19 11:24:18 +0700

    Add test for sumEvenNumbers method in Calculator

--- src/test/java/Test_issue2.java
+++ src/test/java/Test_issue2.java
@@ -1,1 +1,15 @@
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorStatementTest {
+
+    @Test
+    void testSumEvenNumbers() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.sumEvenNumbers(5);
+
+        assertEquals(6, result);
+    }
+}
